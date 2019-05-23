dd775615ac6c9ffcd640abe28a63c83b7196f1cd
MarkDalsaso <mark1@we-is.com>
2019-05-23 14:10:21 -0600

    Initial commit of ECommerce/PhoneShop. Follow-along app for ASP.NET Core 2 and Vue.js

---
+++ ClientApp/boot.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import BootstrapVue from "bootstrap-vue";
+import NProgress from "nprogress";
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+
+//import page components
+import Catalogue from "./pages/Catalogue.vue";
+import Product from "./pages/Product.vue";
+
+const routes = [
+   { path: "/products", component: Catalogue },
+   { path: "/products/:slug", component: Product },
+   { path: "*", redirect: "/products" }   
+];
+
+const router = new VueRouter({ mode: "history", routes: routes});
+
+router.beforeEach((to, from, next) => {
+   NProgress.start();
+  //  setTimeout(function () {
+  // }, 10000);
+   next();
+ });
+ router.afterEach((to, from) => {
+   NProgress.done();
+ });
+
+new Vue({
+    el: '#app-root',
+    router: router,
+    render: h => h(require('./components/App.vue'))
+});
---
+++ ClientApp/components/catalogue/RangeFilter.vue
@@ -0,0 +1,79 @@
+<template>
+
+<!-- NOTE: MRD @callback="filter" was not working, @change="filter" instead -->
+  <div class="slider">
+    <vue-slider 
+      @change="filter"
+      :lazy=true
+      :tooltip="'always'"
+      :value="value"
+      :formatter="formatter" 
+      :min="min"
+      :max="max"
+      :interval="interval || 1"
+      width="90%">
+    </vue-slider>
+  </div>
+</template>
+
+<script>
+import vueSlider from "vue-slider-component";
+// NOTE: MRD also add "vue-slider-component/theme/default.css" to vender array prop in webpack.config.vendor.js
+
+export default {
+  name: "range-filter",
+  //data () { return {}}, // for testing
+  components: {
+    vueSlider
+  },
+  props: {
+    min: {
+      type: Number,
+      required: true
+    },
+    max: {
+      type: Number,
+      required: true
+    },
+    interval: {
+      type: Number
+    },
+    formatter: {
+      type: String,
+      required: true
+    },
+    minQueryKey: {
+      type: String,
+      required: true
+    },
+    maxQueryKey: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    value() {
+      let rtn = [
+        this.$route.query[this.minQueryKey] || this.min,
+        this.$route.query[this.maxQueryKey] || this.max
+      ]
+      //console.log("init. val:" + rtn)   // testing
+      return rtn;
+    }
+  },
+  methods: {
+    filter(values) {
+      let query = Object.assign({}, this.$route.query);
+      query[this.minQueryKey] = values[0];
+      query[this.maxQueryKey] = values[1];
+      this.$router.push({ query: query });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.slider {
+  padding: 35px 0 10px 10px;
+}
+</style>
